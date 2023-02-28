f49824e01c07b5fb4769788964f63eb2b6f595e3
rafaelgarcia-dev <rafa48429@gmail.com>
2023-02-27 22:01:35 -0300

    Projeto Money

---
+++ scripts.js
@@ -0,0 +1,54 @@
+const button = document.getElementById("convert-botton")
+const select = document.getElementById("change-Currency")
+
+const dolar = 5.2
+const euro = 5.4
+
+const convertValues = () => {
+  const inputReais = document.getElementById('input-real').value
+  const realValue = document.getElementById('real-value')
+  const dolarValue = document.getElementById('dolar-value')
+
+  realValue.innerHTML = new Intl.NumberFormat('pt-br',
+    { style: 'currency', currency: 'BRL' }
+  ).format(inputReais);
+
+  if (select.value === "US$ Dólar Americano") {
+   dolarValue.innerHTML = new Intl.NumberFormat('en-US',
+    { style: 'currency', currency: 'USD' }
+  ).format(inputReais / dolar);
+}
+  if (select.value === "€ Euro") {
+   dolarValue.innerHTML = new Intl.NumberFormat('de-DE',
+    { style: 'currency', currency: 'EUR' }
+  ).format(inputReais / euro)
+}
+}
+
+const changeCurrency = () => {
+  const pDolar = document.getElementById("p-dolar")
+  const imageEua = document.getElementById("image-eua")
+
+  
+  if (select.value === "US$ Dólar Americano") {
+    pDolar.innerHTML = "Dólar Americano"
+    imageEua.src = ("./assets/estados-unidos (1) 1.png")
+}
+
+  if (select.value === '€ Euro') {
+    pDolar.innerHTML = "Euro"
+    imageEua.src = ("./assets/Design sem nome 1.png")
+
+  }
+  convertValues()
+  
+ }
+
+ 
+
+
+
+
+button.addEventListener('click', convertValues)
+select.addEventListener("change", changeCurrency)
+
